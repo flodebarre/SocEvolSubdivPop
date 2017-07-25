f5c49648ff05252864f5342a1d1662333cf210ef
flodebarre <florence.debarre@normalesup.org>
2017-07-25 20:43:09 +0200

    working on SI, incl Mathematica

--- Programs/C/htg-island_base.c
+++ Programs/C/htg-island_base.c
@@ -23,8 +23,9 @@
 
 #define MEANDEMESIZE 3 // Average number of individuals per deme on top of focal
 #define VARDEMESIZE 0.75 // Variance of the number of individuals per deme
-#define NBINOM 4 // Binomial parameter such that n p = m (other combination is n=5, p=0.8)
-#define NDEMES 30   // Number of demes
+#define NBINOM 4 // Binomial parameter such that n p +2 = m (other combination is n=5, p=0.8)
+#define PBINOM 0.5 // Binomial parameter such that n p + 2 = m
+#define NDEMES 15   // Number of demes
 
 /* Other parameters */
 int pop[NDEMES]; // Population vector, counting the number of A individuals in each deme
@@ -172,11 +173,11 @@ void GenerateDemeSizes(void)
   if(HTG == 1){
     /* DEME SIZES ARE NOT ALL THE SAME */
     /* Define the parameters of the Binomial distribution used to draw deme sizes */
-    pbinom = ((double) (MEANDEMESIZE - VARDEMESIZE)/MEANDEMESIZE);
+    pbinom = PBINOM;
     for(ideme = 0; ideme<NDEMES; ideme++){
       // For each deme, initialize deme size, and then draw number
-      // Put at least one individual in each deme
-      demesize[ideme] = 1;
+      // Put at least two individuals in each deme
+      demesize[ideme] = 2;
       for(iindiv = 0; iindiv<NBINOM; iindiv++){
         // Consider each individual and see whether it goes to the focal deme
         // This means that we draw WITH REPLACEMENT but it is fine.
